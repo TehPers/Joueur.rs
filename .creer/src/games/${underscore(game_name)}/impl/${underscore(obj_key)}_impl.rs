060fac68bda71bb53388cb55b50c9265943b9d6b
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-02-15 08:41:56 -0600

    Update creer templates with base impl logic

--- .creer/src/games/${underscore(game_name)}/impl/${underscore(obj_key)}_impl.rs
+++ .creer/src/games/${underscore(game_name)}/impl/${underscore(obj_key)}_impl.rs
@@ -20,5 +20,73 @@ impl base::Game for GameImpl {
     }
 }
 % else:
-// TODO: do this implimentation
+<%include file='functions.noCreer' />${shared['rs']['imports'](obj, obj_key, [obj_key] + obj['parentClasses'])}
+<%
+objs = [(obj_key, obj)] + [ (n, game_objs[n]) for n in obj['parentClasses'] ]
+objs = [r for r in reversed(objs)]
+def check_has_game_obj():
+    for parent in obj:
+        for attr in obj['attributes'].values():
+            if attr['type']['is_game_object']:
+                return True
+    return False
+
+def field_name_for(attr_name, attr):
+    field_name = underscore(attr_name)
+    if attr['type']['is_game_object']:
+        field_name += '_id'
+    return field_name
+
+has_game_obj = check_has_game_obj()
+ref = "<'a>" if has_game_obj else ''
+%>
+pub struct ${obj_key}Impl${ref} {
+    pub game_manager: GameManagerImpl,
+
+%   for i, parent in enumerate(objs):
+%       if i > 0:
+
+%       endif
+%       for attr_name in parent[1]['attribute_names']:
+<%
+            attr = parent[1]['attributes'][attr_name]
+            field_name = field_name_for(attr_name, attr)
+%>    pub ${field_name}_impl: ${shared['rs']['type'](attr['type'], True)},
+%       endfor
+%   endfor
+}
+
+%   for i, parent in enumerate(objs):
+%       if i > 0:
+
+%       endif
+impl ${parent[0]} for ${obj_key}Impl {
+%       for j, attr_name in enumerate(parent[1]['attribute_names']):
+%           if j > 0:
+
+%           endif
+<%
+            attr = parent[1]['attributes'][attr_name]
+            attr_type = attr['type']
+            field_name = field_name_for(attr_name, attr)
+            fn_body = ''
+            if attr_type['is_game_object']:
+                fn_body = """        let game_object = self.game_manager.get(&self.{impl})';
+        let {class_name} = game_object.as_{class_name};
+"""
+                if attr_type['nullable']:
+                    fn_body += '        return {};'.format(class_name)
+                else:
+                    fn_body += """        if {class_name}.is_none() {{
+            self.game_manager.handle_no_game_object(&self.{impl}, "{class_name}");
+        }}
+        return {class_name}.unwrap();
+"""
+
+%>    fn ${underscore(attr_name)}(&self): ${shared['rs']['type'](attr_type)} {
+        return self.${field_name}_impl${'.unwrap()' if attr_type['is_game_object'] and not attr_type['nullable'] else ''};
+    }
+%       endfor
+}
+%   endfor
 % endif
--- .creer/src/games/${underscore(game_name)}/impl/game_manager_impl.rs
+++ .creer/src/games/${underscore(game_name)}/impl/game_manager_impl.rs
@@ -1,5 +1,8 @@
-use crate::base::{Game, GameManager};
+<%include file='functions.noCreer' />use crate::base::{Game, GameManager};
 use crate::games::${underscore(game_name)}::r#impl::{GameImpl};
+% for game_obj_name in game_obj_names:
+use crate::games::${underscore(game_name)}::r#impl::{${game_obj_name}Impl};
+% endfor
 use crate::games::${underscore(game_name)}::ai::{PLAYER_NAME};
 
 // pub static GAME_NAME: &str = "${game_name}";
@@ -7,17 +10,56 @@ pub static GAME_VERSION: &str = "${game_version}";
 
 pub struct GameManagerImpl {
     game: GameImpl,
+    game_objects: std::collections::HashMap<String, Box<GameObject>>,
 }
 
 impl GameManagerImpl {
     pub fn new() -> GameManagerImpl {
         return GameManagerImpl{
             game: GameImpl::new(),
+            game_objects: std::collections::HashMap::new(),
         };
     }
 }
 
+impl GameManagerImpl {
+% for i, game_obj_name in enumerate(game_obj_names):
+%   if i > 0:
+
+%   endif
+<%
+    game_obj = game_objs[game_obj_name]
+    all = game_obj['parentClasses'] + [game_obj_name]
+%>    fn new_${underscore(game_obj_name)}(&self) -> dyn ${game_obj_name} {
+        return ${game_obj_name}Impl {
+%   for class_name in all:
+%       for attr_name in game_objs[class_name]['attribute_names']:
+<%
+            attr = game_objs[class_name]['attributes'][attr_name]
+%>          ${attr_name}: ${shared['rs']['default'](attr['type'])},
+%       endfor
+%   endfor
+        };
+    },
+% endfor
+}
+
 impl GameManager for GameManagerImpl {
+    fn ensure_game_object(&mut self, id: &str, game_object_name: &str) -> Optional<bool> {
+        if self.game_objects.contains_key(id) {
+            return Some(false);
+        }
+
+        let new_game_object = match game_object_name {
+% for game_obj_name in game_obj_names:
+            "${game_obj_name}" => self.new_${underscore(game_obj_name)},
+% endfor
+            _ => return None,
+        }
+
+        Some(true)
+    }
+
     fn get_game<'a>(&'a self) -> &'a dyn Game {
         return &self.game;
     }
